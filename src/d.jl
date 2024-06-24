f4c97f5d22c4f514737e142f3383751ceee089ad
afterDark9091 <afterDark9091@protonmail.com>
2024-06-24 21:05:27 +0300

    fixed layout issues

---
+++ src/d.jl
@@ -0,0 +1,19 @@
+#module D
+#export dfunc
+
+include("a.jl")
+#using .A
+
+include("b.jl")
+#using .B
+
+include("c.jl")
+#using .C
+
+function dfunc()
+    a=Astruct()
+    bfunc(a)
+    cfunc(a)
+end
+
+#end
